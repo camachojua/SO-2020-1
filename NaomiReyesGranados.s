cd7a366f168d241eee1b9f0660eeec46c8e32d4a
NaomiReyes <nreyes@ciencias.unam.mx>
2019-08-21 22:11:28 -0500

    Listo

--- NaomiReyesGranados.s
+++ NaomiReyesGranados.s
@@ -8,14 +8,15 @@ _start:
 
 while:
 	div %rbx #Dividimos nuestros numeros
-	cmp $0, %rdx #Vemos si el residuo es 0
 	mov %rdx, %r8 #Guardamos el residuo en r8
 	mov %rbx, %rax #renombramos al segundo numero por el primero
+	mov %rax, %rcx
 	mov $0, %rdx #renombramos para que rdx vuelva a ser 0
 	mov %r8, %rbx #ponemos nuestro residuo como segundo numero en la div
+	cmp $0, %r8 #Vemos si el residuo es 0
 	jne while
 
 	mov $60, %rax
-	mov %rdx, %rdi #devolvemos el ultimo residuo diferente de 0
+	mov %rcx, %rdi #devolvemos el ultimo residuo diferente de 0
 	syscall
 	
