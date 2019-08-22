70a358c9df92bb0d79b78d02cc000215d42ee393
SadAlisa <47436406+YairCorrea@users.noreply.github.com>
2019-08-22 02:02:29 -0500

    Update yairCorrea.s

--- yairCorrea.s
+++ yairCorrea.s
@@ -1,40 +1,27 @@
-	.file	"yaMeDoy.c"
-	.text
-	.globl	mcd
-	.type	mcd, @function
-mcd:
-.LFB6:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	%edi, -20(%rbp)
-	movl	%esi, -24(%rbp)
-	cmpl	$0, -24(%rbp)
-	jne	.L2
-	movl	-20(%rbp), %eax
-	jmp	.L3
-.L2:
-	movl	-24(%rbp), %edx
-	movl	-20(%rbp), %eax
-	movl	%edx, %esi
-	movl	%eax, %edi
-	call	div@PLT
-	movq	%rax, -8(%rbp)
-	movl	-4(%rbp), %edx
-	movl	-24(%rbp), %eax
-	movl	%edx, %esi
-	movl	%eax, %edi
-	call	mcd
-.L3:
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE6:
-	.size	mcd, .-mcd
-	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
-	.section	.note.GNU-stack,"",@progbits
+#int mcd(int a,int b){
+#	if(b==0){
+#		return a;
+#	}
+#	return mcd(b,a%b);
+#}Codigo de esto en la zona marginada de C.
+.global _start
+.text
+_start:
+	mov $666, %rax	# Inserta valor
+	mov $33, %rdi #Inserta valor
+	call euclides
+	call getOut
+euclides:
+	cmp $0,%rdi	#Resta rdi a 0 y altera la bandera
+	jne getOut			#Si la resta es cero, es decir, rdi es 0 y sale. Realmente no la guarda.
+	jmp recursa
+recursa:
+		mov $0,%rdx
+		div %rdi #Divide lo que este en rax entre lo que este en rdi
+		mov %rdi,%rax #Llegados a este punto ya tenemos que hacer recursion, asi que movemos b a el lugar de a... que ya ni esta, pero no nos importa el cociente
+		mov %rdx,%rdi #Movemos el residuo a el lugar donde estaba b.
+		call euclides #Vuelve a llamarse.
+getOut:
+	mov %rax,%rdi #No reportamos en rax tristemenete, pero en rdi... close enough... I guess
+	mov $60,%rax #calls exit
+	syscall
