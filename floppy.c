50492aba6fbc6b3698295004503e16ca4cb8f833
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2019-10-15 22:07:49 -0500

    Se menciona cómo utilizar C.

--- floppy.c
+++ floppy.c
@@ -1,9 +1,15 @@
 __asm__(".code16\n");
 __asm__("jmpl $0x0000, $main\n");
 
+void imprimeCadena(const char *cadena)
+{
+  while(*cadena) {
+    __asm__ __volatile__("int $0x10" : : "a"(0x0e00 | *cadena), "b"(0x0007));
+    ++cadena;
+  }
+}
+
 void main()
 {
-  __asm__ __volatile__ ("mov $'X'  , %al\n");
-  __asm__ __volatile__ ("mov $0x0e, %ah\n");
-  __asm__ __volatile__ ("int $0x10\n");
+  imprimeCadena("Hola mundo");
 }
