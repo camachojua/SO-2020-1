6ce8241a85de7da932acccbcdd562e42aa713385
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2019-11-26 07:50:31 -0600

    Refactorización.

--- Proyecto/makefile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-as Proyecto.s -o Proyecto.o
-ld -Ttext 0x7c00 --oformat=binary Proyecto.o -o Proyecto.bin
-dd if=/dev/zero of=Proyecto.img bs=512 count=2880
-dd if=Proyecto.bin of=Proyecto.img
-qemu-system-x86_64 -fda Proyecto.img
